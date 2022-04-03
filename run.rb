4dbd17267f5d225ac50ba0d467638a6dc44c97df
Cristian Mocanu <chrimeea@yahoo.com>
2022-04-03 23:57:21 +0300

    bug fixes

--- run.rb
+++ run.rb
@@ -3,16 +3,9 @@ require 'forwardable'
 require_relative 'map'
 require_relative 'tile'
 
-q1_quad = Gandhi::QuadShape.new(Gandhi::Point.new(0, 0), Gandhi::Point.new(10, 10)) 
-q1_tile = Gandhi::Tile.new q1_quad
-q2 = Gandhi::QuadTree.new q1_quad, 2
-q2.insert(q1_tile)
-puts q2.shapes
-
-# q3 = Gandhi::QuadShape.new(Gandhi::Point.new(0, 0), Gandhi::Point.new(1, 1))
-# q2.insert(q3)
-# puts q2.shapes(q1)
-
-# q4 = Gandhi::QuadShape.new(Gandhi::Point.new(9, 9), Gandhi::Point.new(10, 10))
-# q2.insert(q4)
-# puts q2.shapes(q1)
+quad1 = Gandhi::QuadShape.new(Gandhi::Point.new(0, 0), Gandhi::Point.new(10, 10)) 
+tile1 = Gandhi::Tile.new quad1
+tree = Gandhi::QuadTree.new quad1, 2
+tree.insert tile1
+quad2 = Gandhi::QuadShape.new(Gandhi::Point.new(2.5, 2.5), Gandhi::Point.new(7.5, 7.5)) 
+puts tree.shapes quad2
